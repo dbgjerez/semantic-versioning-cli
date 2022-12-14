247f0a176f33c51199cceaed71c2d1b2f8685376
David Borrego <dborrego@redhat.com>
2022-12-14 10:49:13 +0100

    new use case

--- actions/InitAction_test.go
+++ actions/InitAction_test.go
@@ -22,41 +22,53 @@ func TestNewConfig(t *testing.T) {
 			},
 		},
 	}
-	init := InitAction{
-		ArtifactName:    ArtifactName,
-		Major:           Major,
-		Minor:           Minor,
-		Patch:           Patch,
-		SnapshotsEnable: INIT_SNAPSHOTS_ENABLED,
-		SnapshotsKey:    INIT_SNAPSHOTS_KEY,
+	useCases := []InitAction{
+		{
+			ArtifactName:    ArtifactName,
+			Major:           Major,
+			Minor:           Minor,
+			Patch:           Patch,
+			SnapshotsEnable: INIT_SNAPSHOTS_ENABLED,
+			SnapshotsKey:    INIT_SNAPSHOTS_KEY,
+		},
+		{
+			ArtifactName:    ArtifactName,
+			Major:           Major,
+			Minor:           Minor,
+			Patch:           Patch,
+			SnapshotsEnable: INIT_SNAPSHOTS_ENABLED,
+			SnapshotsKey:    "",
+		},
 	}
-	got, err := init.NewConfig()
-	if err != nil {
-		t.Errorf("Don't expected fails!")
-	}
-	if want.Config.Snapshots.Key != got.Config.Snapshots.Key ||
-		want.Config.Snapshots.Key != INIT_SNAPSHOTS_KEY ||
-		got.Config.Snapshots.Key != INIT_SNAPSHOTS_KEY {
-		t.Errorf("Want ArtifactName %s and got %s", INIT_SNAPSHOTS_KEY, got.Config.Snapshots.Key)
-	}
-	if want.Data.ArtifactName != got.Data.ArtifactName ||
-		want.Data.ArtifactName != ArtifactName ||
-		got.Data.ArtifactName != ArtifactName {
-		t.Errorf("Want ArtifactName %s and got %s", ArtifactName, got.Data.ArtifactName)
-	}
-	if want.Data.Version.Major != got.Data.Version.Major ||
-		want.Data.Version.Major != Major ||
-		got.Data.Version.Major != Major {
-		t.Errorf("Want major version %d and got %d", Major, got.Data.Version.Major)
-	}
-	if want.Data.Version.Minor != got.Data.Version.Minor ||
-		want.Data.Version.Minor != Minor ||
-		got.Data.Version.Minor != Minor {
-		t.Errorf("Want minor version %d and got %d", Minor, got.Data.Version.Minor)
-	}
-	if want.Data.Version.Patch != got.Data.Version.Patch ||
-		want.Data.Version.Patch != Patch ||
-		got.Data.Version.Patch != Patch {
-		t.Errorf("Want patch version %d and got %d", Patch, got.Data.Version.Patch)
+	for _, uc := range useCases {
+		got, err := uc.NewConfig()
+		if err != nil {
+			t.Errorf("Don't expected fails!")
+		}
+		if want.Config.Snapshots.Key != got.Config.Snapshots.Key ||
+			want.Config.Snapshots.Key != INIT_SNAPSHOTS_KEY ||
+			got.Config.Snapshots.Key != INIT_SNAPSHOTS_KEY {
+			t.Errorf("Want ArtifactName %s and got %s", INIT_SNAPSHOTS_KEY, got.Config.Snapshots.Key)
+		}
+		if want.Data.ArtifactName != got.Data.ArtifactName ||
+			want.Data.ArtifactName != ArtifactName ||
+			got.Data.ArtifactName != ArtifactName {
+			t.Errorf("Want ArtifactName %s and got %s", ArtifactName, got.Data.ArtifactName)
+		}
+		if want.Data.Version.Major != got.Data.Version.Major ||
+			want.Data.Version.Major != Major ||
+			got.Data.Version.Major != Major {
+			t.Errorf("Want major version %d and got %d", Major, got.Data.Version.Major)
+		}
+		if want.Data.Version.Minor != got.Data.Version.Minor ||
+			want.Data.Version.Minor != Minor ||
+			got.Data.Version.Minor != Minor {
+			t.Errorf("Want minor version %d and got %d", Minor, got.Data.Version.Minor)
+		}
+		if want.Data.Version.Patch != got.Data.Version.Patch ||
+			want.Data.Version.Patch != Patch ||
+			got.Data.Version.Patch != Patch {
+			t.Errorf("Want patch version %d and got %d", Patch, got.Data.Version.Patch)
+		}
 	}
 }
